8ad03f81594d4c8f500bfcf6c78151031bd36a71
mkhandekar1705 <119282978+mkhandekar1705@users.noreply.github.com>
2022-11-28 23:47:02 +0530

    Add files via upload
    Prime number and star pattern code

---
+++ day2/MyCal.java
@@ -0,0 +1,10 @@
+package week1.day2;
+
+public class MyCal {
+	
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		System.out.println(cal.add(10, 20, 30));
+	}
+
+}
---
+++ day2/PrimeNumber.java
@@ -0,0 +1,35 @@
+package week1.day2;
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter number: ");
+		int no = s.nextInt();
+		int temp = 0;
+		
+		for(int i=2; i<= no-1; i++)
+		{
+			if(no % i == 0)
+			{
+				temp = temp + 1;
+			}
+		}
+
+		if(temp == 0)
+		{
+			System.out.println(no+ " is Prime Numbe ");
+		}
+		else
+		{
+			System.out.println(no+ " is not Prime");
+		}
+		
+	   }
+		
+}
+	
+
+
